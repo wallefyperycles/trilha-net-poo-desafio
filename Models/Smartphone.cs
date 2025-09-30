b2eda96963c8fc2599d7a38a79f000092a6c0e03
Wallefy Perycles <perycleswallefy@gmail.com>
2025-09-29 23:27:51 -0300

    Desafio Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,21 +4,27 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo;
+        protected string IMEI;
+        protected int Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.Clear();
+
+System.Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+//====================================================
+//Delay simulando uma ligação
+//====================================================
+await Task.Delay(2000);
+System.Console.WriteLine(".");
+await Task.Delay(2000);
+Console.Clear();
+System.Console.WriteLine("..");
+await Task.Delay(2000);
+Console.Clear();
+System.Console.WriteLine("...");
+await Task.Delay(2000);
+Console.Clear();
+//===================================================
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n"); // pular linha
+
+System.Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "87654321", modelo: "Modelo 2", imei: "222222222", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
